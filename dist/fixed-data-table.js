f09a3403ab3af1696bfb07d09ccc9e0ce156a981
steen hansen <steen_hansen@yahoo.com>
2016-05-18 22:42:36 -0700

    Changed Node.js server response to assuming Chrome/FireFox/Opera/IE browser

--- dist/fixed-data-table.js
+++ dist/fixed-data-table.js
@@ -3623,29 +3623,35 @@ return /******/ (function(modules) { // webpackBootstrap
 	var BACKFACE_VISIBILITY = getVendorPrefixedName('backfaceVisibility');
 
 	var translateDOMPositionXY = (function () {
-	  if (BrowserSupportCore.hasCSSTransforms()) {
-	    var ua = global.window ? global.window.navigator.userAgent : 'UNKNOWN';
-	    var isSafari = /Safari\//.test(ua) && !/Chrome\//.test(ua);
-	    // It appears that Safari messes up the composition order
-	    // of GPU-accelerated layers
-	    // (see bug https://bugs.webkit.org/show_bug.cgi?id=61824).
-	    // Use 2D translation instead.
-	    if (!isSafari && BrowserSupportCore.hasCSS3DTransforms()) {
-	      return function ( /*object*/style, /*number*/x, /*number*/y) {
-	        style[TRANSFORM] = 'translate3d(' + x + 'px,' + y + 'px,0)';
-	        style[BACKFACE_VISIBILITY] = 'hidden';
-	      };
-	    } else {
-	      return function ( /*object*/style, /*number*/x, /*number*/y) {
-	        style[TRANSFORM] = 'translate(' + x + 'px,' + y + 'px)';
-	      };
-	    }
-	  } else {
-	    return function ( /*object*/style, /*number*/x, /*number*/y) {
-	      style.left = x + 'px';
-	      style.top = y + 'px';
-	    };
-	  }
+	  var is_node_js = (typeof window == "undefined" )
+      if (is_node_js){
+        return function ( /*object*/style, /*number*/x, /*number*/y) {
+          style['transform'] = 'translate3d(' + x + 'px,' + y + 'px,0)';
+          style['backfaceVisibility'] = 'hidden';
+        };
+      }else if (BrowserSupportCore.hasCSSTransforms()) {
+        var ua = global.window ? global.window.navigator.userAgent : 'UNKNOWN';
+        var isSafari = /Safari\//.test(ua) && !/Chrome\//.test(ua);
+        // It appears that Safari messes up the composition order
+        // of GPU-accelerated layers
+        // (see bug https://bugs.webkit.org/show_bug.cgi?id=61824).
+        // Use 2D translation instead.
+        if (!isSafari && BrowserSupportCore.hasCSS3DTransforms()) {
+          return function ( /*object*/style, /*number*/x, /*number*/y) {
+            style[TRANSFORM] = 'translate3d(' + x + 'px,' + y + 'px,0)';
+            style[BACKFACE_VISIBILITY] = 'hidden';
+          };
+        } else {
+          return function ( /*object*/style, /*number*/x, /*number*/y) {
+            style[TRANSFORM] = 'translate(' + x + 'px,' + y + 'px)';
+          };
+        }
+      } else {
+        return function ( /*object*/style, /*number*/x, /*number*/y) {
+          style.left = x + 'px';
+          style.top = y + 'px';
+        };
+      }
 	})();
 
 	module.exports = translateDOMPositionXY;
